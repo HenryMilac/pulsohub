aeca9e113165a065c890d66511942c628a28a4fd
henryramirezdatamatrix <henrymilacprog@gmail.com>
2025-07-31 17:19:52 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('principal');
+});
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::get('/login', function () {
+    return view('auth.login');
+});
