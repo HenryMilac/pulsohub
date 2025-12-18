ada01cf4715558f464a4a1747a3b390281d5d54d
Henry Ramirez <henrymilacprog@gmail.com>
2025-12-18 11:55:15 -0500

    fix: like/unlike post

--- app/Livewire/LikePost.php
+++ app/Livewire/LikePost.php
@@ -10,14 +10,20 @@ class LikePost extends Component
 
     public function like()
     {
-        if($this->post->checkLike(auth()->user())){
-            $this->post->likes()->where('post_id', $this->post->id)->delete();
-        }else{
+        if (!$this->post->checkLike(auth()->user())) {
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id,
             ]);
+            $this->post->refresh();
         }
-        $this->post->refresh();
+    }
+
+    public function unlike()
+    {
+        if ($this->post->checkLike(auth()->user())) {
+            $this->post->likes()->where('user_id', auth()->user()->id)->delete();
+            $this->post->refresh();
+        }
     }
 
     public function render()
